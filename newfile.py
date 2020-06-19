053e70541ee75c6252e89073e440f2d342d19a05
bhavya-creator <67138149+bhavya-creator@users.noreply.github.com>
2020-06-19 21:19:29 +0530

    321810304052

---
+++ newfile.py
@@ -0,0 +1,45 @@
+#SIMPLE  INTEREST
+p=2000
+t=2
+r=5
+SI=(p*t*r)/100
+print("simple interest:",SI)
+
+#AREA OF CIRCLE
+r=4
+Area=3.142*r*r
+print("Area of circle:",Area)
+
+#AREA OF TRIANGLE
+h,b=8,4
+a=(h*b)/2
+print("Area of triangle:",a)
+
+#TEMPARATURE FROM CELSUIS TO FAHRENHEIT
+tc=20
+tf=tc*1.8+32
+print("temperature in fahrenheit:",tf)
+
+#AREA OF RECTANGLE
+w=10
+l=5
+print("area of rectangle:",l*w)
+
+#PERIMETER OF SQUARE
+a1=4
+p=4*a
+print("Perimeter of square:",p)
+
+#CIRCUMFERENCE OF CIRCLE
+r1=3
+c=2*3.142*r1
+print("circumference of circle:",c)
+
+#SWAPPING OF TWO NUMBERS
+x=8
+y=2
+print("value of x and y before swpping",x,y)
+x=x+y
+y=x-y
+x=x-y
+print("after swapping of number x and y:",x,y)
